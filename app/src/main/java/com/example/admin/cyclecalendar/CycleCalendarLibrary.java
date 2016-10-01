649e7f6850b6281fe5718ecc8fa945f77906b571
Albert Dale Palacio <palacioalbertdale@gmail.com>
2016-10-01 18:46:41 +0800

    added working cycle calendar library

---
+++ app/src/main/java/com/example/admin/cyclecalendar/CycleCalendarLibrary.java
@@ -0,0 +1,137 @@
+package com.example.admin.cyclecalendar;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.google.common.io.Files;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.util.Date;
+
+/**
+ * This class is for the static methods used in the Cycle Calendar project
+ * v1.0 - initial database handling
+ */
+public class CycleCalendarLibrary {
+
+    /**
+     * CFG filename
+     */
+    public static final String CONFIG = "cyclecalendar_config.txt";
+
+    /**
+     * Android format standard
+     */
+    public static final String FORMAT = "UTF-8";
+
+    /**
+     * This method is for saving name to the local storage initially
+     */
+    public static void saveName(String name, Context context) {
+        File path = context.getFilesDir();
+        File file = new File(path, CONFIG);
+
+        //empties database
+        try {
+            new PrintWriter(file).close();
+            Files.write(name, file, Charset.forName(FORMAT));
+        } catch (IOException e) {
+            Log.e(null, "saveName error");
+        }
+    }
+
+    /**
+     * This method is for saving data to the local storage
+     */
+    public static void saveData(Date[] periodDate, int[] periodChart, Context context) {
+        DatabaseAttributes pa = new DatabaseAttributes();
+        File path = context.getFilesDir();
+        File file = new File(path, CONFIG);
+
+        //empties database
+        try {
+            new PrintWriter(file).close();
+        } catch (IOException e) {
+            Log.e(null, "saveName error");
+        }
+
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+
+        pa.periodDate = periodDate;
+        pa.periodChart = periodChart;
+
+        try {
+            Files.write(gson.toJson(pa), file, Charset.forName(FORMAT));
+        } catch (IOException e) {
+            Log.e(null, "saveName error");
+        }
+    }
+
+    /**
+     * Initializes dates that have periods
+     */
+    public static Date[] initializeDates(Context context) {
+        Gson gson = new Gson();
+        File path = context.getFilesDir();
+        File file = new File(path, CONFIG);
+        DatabaseAttributes da = null;
+        try {
+            String contents = Files.toString(file, Charset.forName(FORMAT));
+            da = gson.fromJson(contents, DatabaseAttributes.class);
+        } catch (IOException e) {
+            Log.e(null, "saveName error");
+        }
+        return da.periodDate;
+    }
+
+    /**
+     * Initializes days that have periods
+     */
+    public static int[] initializeChart(Context context) {
+        Gson gson = new Gson();
+        File path = context.getFilesDir();
+        File file = new File(path, CONFIG);
+        DatabaseAttributes da = null;
+        try {
+            String contents = Files.toString(file, Charset.forName(FORMAT));
+            da = gson.fromJson(contents, DatabaseAttributes.class);
+        } catch (IOException e) {
+            Log.e(null, "saveName error");
+        }
+        return da.periodChart;
+    }
+
+    /**
+     * gets the name from the database
+     * if name == null then do saveName method
+     */
+
+    public static String getName(Context context) {
+        File path = context.getFilesDir();
+        File file = new File(path, CONFIG);
+        try {
+            return Files.toString(file, Charset.forName(FORMAT));
+        } catch (IOException e) {
+            Log.e(null, "saveName error");
+        }
+        return null;
+    }
+
+}
+
+/**
+ * database values
+ */
+class DatabaseAttributes {
+    //for calendar
+    public Date[] periodDate;
+    //for graph
+    public int[] periodChart;
+}
+
